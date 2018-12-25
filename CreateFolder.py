45af2a4046ac33fca80fe3aa9b040e4f6eecfd71
ChengCheng <345199390@qq.com>
2018-12-25 16:21:34 +0800

    2018-12-25 16:21:29

---
+++ CreateFolder.py
@@ -0,0 +1,26 @@
+import os
+
+print(os.getcwd())
+
+
+# folder = os.getcwd()[:-4] + 'new_folder\\test\\'
+# os.mkdir('umei1')
+# folder = os.path.exists('umei1')
+# print(folder)
+# 获取此py文件路径，在此路径选创建在new_folder文件夹中的test文件夹
+
+# if not os.path.exists(folder):
+# os.makedirs(folder)
+
+
+def create_folder(file_name):
+    '''新建文件夹'''
+    if os.path.exists(file_name):
+        print("文件夹已存在")
+    else:
+        os.mkdir(file_name)
+        print('文件夹%s创建成功', file_name)
+
+
+if __name__ == '__main__':
+    create_folder('umei1')
